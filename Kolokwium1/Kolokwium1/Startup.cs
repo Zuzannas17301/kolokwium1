2beaa42ec8528eb39baa5880d910c789df92b564
zuzannas17301 <s17301@pjwstk.edu.pl>
2020-04-27 17:22:48 +0200

    Kolokwium1

--- Kolokwium1/Kolokwium1/Startup.cs
+++ Kolokwium1/Kolokwium1/Startup.cs
@@ -26,7 +26,8 @@ namespace Kolokwium1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IDbService, SqlServerService>();
+          
+            services.AddTransient<IDbService, SqlServerService>();
             services.AddSingleton<IDeleteService, SqlDeleteServerService>();
             services.AddControllers();
         }
